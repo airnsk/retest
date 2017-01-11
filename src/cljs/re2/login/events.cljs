4cec9ccac2120f6ed05c8cae020a1be224e130d8
airnsk <airnsk@mail.ru>
2017-01-11 10:20:36 +0300

    test http login

--- src/cljs/re2/login/events.cljs
+++ src/cljs/re2/login/events.cljs
@@ -1,8 +1,23 @@
 (ns re2.login.events
     (:require [re-frame.core :as re-frame]
+              [ajax.core :as ajax]
+              [day8.re-frame.http-fx]
               [re2.login.db :as db]))
 
 (re-frame/reg-event-db
  :initialize-db/login
  (fn  [rootdb _]
    (assoc rootdb :logindb db/default-db)))
+
+ (re-frame/reg-event-fx        ;; <-- note the `-fx` extension
+   :http-login        ;; <-- the event id
+   (fn                ;; <-- the handler function
+     [{db :db} _]     ;; <-- 1st argument is coeffect, from which we extract db
+
+     ;; we return a map of (side) effects
+     {:http-xhrio {:method          :get
+                   :uri             "http://json.my-endpoint.com/blah"
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:process-response]
+                   :on-failure      [:bad-response]}
+      :db  (assoc db :loading? true)}))
